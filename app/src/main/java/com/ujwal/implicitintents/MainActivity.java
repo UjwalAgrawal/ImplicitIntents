dd9889215bef56ad028e9cd5c162457c61f21778
UjwalAgrawal <uvagrawal123@gmail.com>
2020-05-06 23:47:02 +0530

    Few basic implicit examples of Android Studio

--- app/src/main/java/com/ujwal/implicitintents/MainActivity.java
+++ app/src/main/java/com/ujwal/implicitintents/MainActivity.java
@@ -2,13 +2,57 @@ package com.ujwal.implicitintents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCall, btnCallFriend, btnMap, btnWeb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnCall = findViewById(R.id.btnCall);
+        btnCallFriend = findViewById(R.id.btnCallFriend);
+        btnMap = findViewById(R.id.btnMap);
+        btnWeb = findViewById(R.id.btnWeb);
+
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                startActivity(intent);
+            }
+        });
+
+        btnCallFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:9876543210"));
+                startActivity(intent);
+            }
+        });
+
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=Abhinav Vidya Mandir, Bhayandar, Mumbai"));
+                startActivity(intent);
+
+            }
+        });
+
+        btnWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://wikipedia.org/wiki/india"));
+                startActivity(intent);
+
+            }
+        });
     }
 }
